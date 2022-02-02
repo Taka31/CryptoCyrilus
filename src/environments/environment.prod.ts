f3a95c38f799768147152d47192f5f9ab18c48d3
Cyril Menard <cyril.menard@neo-soft.fr>
2022-02-02 16:53:12 +0100

    Second step2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBQ6TzV4I2WMubGzEqeTHAGyShNMUBn-50",
+    authDomain: "cyrilus-cryptus.firebaseapp.com",
+    projectId: "cyrilus-cryptus",
+    storageBucket: "cyrilus-cryptus.appspot.com",
+    messagingSenderId: "1072115900123",
+    appId: "1:1072115900123:web:151182a7e3d66c049436fe"
+  }
 };
